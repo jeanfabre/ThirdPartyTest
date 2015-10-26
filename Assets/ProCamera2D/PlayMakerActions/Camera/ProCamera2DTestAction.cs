e517cec6e1fbff588dd2279dd29a2354d57e99cd
jeanfabre <contact@fabrejean.net>
2015-10-26 14:03:37 +0300

    better test action

---
+++ Assets/ProCamera2D/PlayMakerActions/Camera/ProCamera2DTestAction.cs
@@ -0,0 +1,16 @@
+#if PC2D_PLAYMAKER_SUPPORT
+
+// __ECO__ __PROCAMERA2D__ __ACTION
+
+using Com.LuisPedroFonseca.ProCamera2D;
+using HutongGames.PlayMaker;
+using TooltipAttribute = HutongGames.PlayMaker.TooltipAttribute;
+
+[ActionCategory(ActionCategory.Camera)]
+[Tooltip("Add a target for the camera to follow.")]
+public class ProCamera2DAddTargetAction : FsmStateAction 
+{
+
+}
+
+#endif
